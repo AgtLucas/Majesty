71a65f14b6ab5c93a5aab9bb69054d2db216527f
Lucas <lucas@agtlucas.com>
2016-02-22 23:14:53 -0300

    Standard js

--- index.android.js
+++ index.android.js
@@ -2,17 +2,18 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
-'use strict';
+'use strict'
+
 import React, {
   AppRegistry,
   Component,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
 
 class Majesty extends Component {
-  render() {
+  render () {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -25,7 +26,7 @@ class Majesty extends Component {
           Shake or press menu button for dev menu
         </Text>
       </View>
-    );
+    )
   }
 }
 
@@ -34,18 +35,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
 
-AppRegistry.registerComponent('Majesty', () => Majesty);
+AppRegistry.registerComponent('Majesty', () => Majesty)
