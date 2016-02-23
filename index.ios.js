d8987bf9f1f4aedf22f441106186a1861bea2380
Lucas <lucas@agtlucas.com>
2016-02-22 23:25:17 -0300

    Update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -12,19 +12,30 @@ import React, {
   View
 } from 'react-native'
 
+import Realm from 'realm'
+
 class Majesty extends Component {
   render () {
+    var realm = new Realm({
+      schema: [{
+        name: 'Cat',
+        properties: {
+          name: 'string'
+        }
+      }]
+    })
+
+    realm.write(() => {
+      realm.create('Cat', ['Lynx'])
+    })
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          Count of Cats in Realm: {realm.objects('Cat').length}
         </Text>
       </View>
     )
